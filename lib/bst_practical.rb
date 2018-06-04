e929efbe15987769121a6669a9a1f5a88e3a08cc
Cory Kane <ckane30691@gmail.com>
2018-06-04 15:08:51 -0700

    Write kth_largest

--- lib/bst_practical.rb
+++ lib/bst_practical.rb
@@ -1,3 +1,7 @@
+require "binary_search_tree"
 
 def kth_largest(tree_node, k)
+  bst = BinarySearchTree.new
+  traversed = bst.in_order_traversal(tree_node)
+  bst.find(traversed[traversed.length-k], tree_node)
 end
